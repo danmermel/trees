915087eaca3adeee7b1fa886aed92d814250aa3b
Daniel Mermelstein <danmermel@gmail.com>
2024-04-12 12:09:59 +0100

    added lambdas to get by species and by geo

---
+++ lambda/getbygeo.js
@@ -0,0 +1,61 @@
+const AWS = require('aws-sdk')
+const TABLE = process.env.TABLE
+const API_KEY = process.env.API_KEY
+const documentClient = new AWS.DynamoDB.DocumentClient()
+
+let body
+let statusCode
+
+const handler = async function (spec) {
+  // first check if the API KEY is correct
+  //   if (!spec.queryStringParameters || !spec.queryStringParameters.apikey || spec.queryStringParameters.apikey !== API_KEY) {
+  //     return { statusCode: 401, body: '{"ok": false}' }
+  //   }
+
+  spec = spec.queryStringParameters || {}
+  //console.log('spec is ', spec)
+
+  // check data
+  if (!spec.geoHash) {
+
+    return { statusCode: 400, body: `{"ok": false, "message": "Missing geoHash parameter"}` }
+
+  }
+
+
+  // get items by geohash
+
+  try {
+
+    const req = {
+      TableName: TABLE,
+      IndexName: 'gsi2',
+      KeyConditions: {
+        GSI2PK: { ComparisonOperator: 'EQ', AttributeValueList: [`geo#${spec.geoHash}`] }
+      }
+    }
+    const response = await documentClient.query(req).promise()
+
+    console.log("response is ", JSON.stringify(response))
+
+    body = {
+      ok: true,
+      trees: response.Items.map((i) => {
+        return i
+      })
+    }
+    statusCode = 200
+  } catch (e) {
+    body = { ok: false, message: 'Query Failed' }
+    statusCode = 500
+  }
+
+  // return API response
+  return {
+    body: JSON.stringify(body),
+    statusCode: statusCode
+  }
+
+}
+
+module.exports = { handler }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -77,3 +77,33 @@ module "getTree" {
 output "getTreeFunctionUrl" {
   value = module.getTree.url
 }
+
+
+
+module "getBySpecies" {
+  source        = "./modules/apicall"
+  function_name = "getbyspecies"
+  role          = aws_iam_role.treesLambdaRole.arn
+  table         = aws_dynamodb_table.treesDb.name
+  layer         = aws_lambda_layer_version.treesLambdaLayer.arn
+  api_key       = random_string.apiKey.result
+}
+
+
+output "getBySpeciesFunctionUrl" {
+  value = module.getBySpecies.url
+}
+
+module "getByGeo" {
+  source        = "./modules/apicall"
+  function_name = "getbygeo"
+  role          = aws_iam_role.treesLambdaRole.arn
+  table         = aws_dynamodb_table.treesDb.name
+  layer         = aws_lambda_layer_version.treesLambdaLayer.arn
+  api_key       = random_string.apiKey.result
+}
+
+
+output "getByGeoFunctionUrl" {
+  value = module.getByGeo.url
+}
