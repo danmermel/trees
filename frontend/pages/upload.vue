8ae5f477d39c855308886fd85d97efd21991b844
Daniel Mermelstein <danmermel@gmail.com>
2024-10-29 11:41:42 +0000

    In this iteration we made sure that trees that don't get submitted to the cloud don't get erased from local storage so that they can be tried again later.
    In order to simulate being offline, use the Network tab in the Inspect tools. Under "No throttling there is an option to go "offline".
    We found out that an API call that fails does not trigger the catch part of the try/catch because it comes back as a success but with a null response.
    
    Signed-off-by: Daniel Mermelstein <danmermel@gmail.com>

--- frontend/pages/upload.vue
+++ frontend/pages/upload.vue
@@ -8,26 +8,43 @@ const treeCount = ref(1);
 treeCount.value = 0;
 
 const submit = async function () {
-    disabled.value = true
+  disabled.value = true;
   //load tree data
   let offlineTrees = localStorage.getItem("offlinetrees");
   offlineTrees = JSON.parse(offlineTrees);
 
   for (const tree of offlineTrees) {
     console.log("writing to cloud");
-    tree.apiKey = auth.value.apiKey
-    const r = await useFetch(
-      `https://whuoepm55me6lmx6dyonsdim3i0xiowx.lambda-url.eu-west-1.on.aws/`,
-      {
-        query: tree,
-        method: "get",
+    tree.apiKey = auth.value.apiKey;
+    try {
+      const r = await useFetch(
+        `https://whuoepm55me6lmx6dyonsdim3i0xiowx.lambda-url.eu-west-1.on.aws/`,
+        {
+          query: tree,
+          method: "get",
+        }
+      );
+      console.log("response is ", r.data.value);
+      if (!r.data.value) {
+        //null response so  tree not submitted
+        tree.submitted = false;
+      } else {
+        tree.submitted = true;
       }
-    );
-    console.log(r.data.value);
-    treeCount.value++;
+      treeCount.value++;
+    } catch (e) {
+      console.log("error adding tree");
+      tree.submitted = false;
+    }
   }
-  // now remove the offline trees from local storage
-  localStorage.removeItem("offlinetrees");
+  // now remove the submitted offline trees from local storage
+  offlineTrees = offlineTrees.filter(function (tree) {
+    return tree.submitted === false;
+  });
+  // now offlineTrees is an array that only contains failed submissions. Write them back to local storage
+  offlineTrees = JSON.stringify(offlineTrees);
+  localStorage.setItem("offlinetrees", offlineTrees);
+  disabled.value = false;
   // create alert
   alert.value.ts = new Date().getTime();
   alert.value.message = "Saved Offline trees to the Cloud";
@@ -36,12 +53,13 @@ const submit = async function () {
 </script>
 
 <template>
-  <p>You have {{ auth.offlineTreesCount }} trees in local storage. Do you want to
-  save to the cloud now?</p>
+  <p>You have {{ auth.offlineTreesCount }} trees in local storage.</p>
+  <p v-if="auth.offlineTreesCount > 0">Do you want to save to the cloud now?</p>
 
-  <v-btn :disabled="disabled" @click="submit">Submit</v-btn>
+  <v-btn v-if="auth.offlineTreesCount > 0" :disabled="disabled" @click="submit">Submit</v-btn>
 
-  <v-progress-linear v-if="disabled"
+  <v-progress-linear
+    v-if="disabled"
     :model-value="treeCount"
     :max="auth.offlineTreesCount"
   ></v-progress-linear>
