df903d7ecc17cd5a1567fb69888a6e710b5f0698
Daniel Mermelstein <danmermel@gmail.com>
2024-12-06 11:15:40 +0000

    In this iteration we added some functionality to the homepage that checks whether the site has changed and therefore the cache needs to be invalidated and the site refreshed
    Signed-off-by: Daniel Mermelstein <danmermel@gmail.com>

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,23 @@
 <script setup>
-  const router = useRouter()
-  const sponsor = ref(0)
-  sponsor.value = ''
+const router = useRouter();
+const sponsor = ref(0);
+sponsor.value = "";
 
-  const viewSponsor = async () => {
-    if (sponsor.value.length > 0) {
-      router.push({ path: `/sponsor-${sponsor.value}` })
-    }
+const { $pwa } = useNuxtApp();
+
+const viewSponsor = async () => {
+  if (sponsor.value.length > 0) {
+    router.push({ path: `/sponsor-${sponsor.value}` });
   }
-
+};
 </script>
 <template>
- <h2>Enter sponsor name</h2>
- <v-text-field label="Sponsor" v-model="sponsor"></v-text-field>
- <v-btn @click="viewSponsor">View Sponsor's Trees</v-btn>
+  <v-alert color="warning" v-show="$pwa.needRefresh">
+    <h2> New content available, click on reload button to update. </h2>
+
+    <v-btn @click="$pwa.updateServiceWorker()">Reload</v-btn>
+  </v-alert>
+  <h2>Enter sponsor name</h2>
+  <v-text-field label="Sponsor" v-model="sponsor"></v-text-field>
+  <v-btn @click="viewSponsor">View Sponsor's Trees</v-btn>
 </template>
